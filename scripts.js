d92a8dd77a27f41be156986cf616871d3da7b421
lucas sant <lusaintdev@gmail.com>
2022-11-03 14:18:38 -0300

    editar ToDo

--- scripts.js
+++ scripts.js
@@ -1,9 +1,15 @@
 const todoForm = document.querySelector('#todo-form');
 const todoTarefa = document.querySelector('#tarefa');
-const todoList = document.querySelector('#todo-list');
-const todoFiltro = document.querySelector('#filtro');
-const todoEdit = document.querySelector('#edit');
-const todoCancelBtn = document.querySelector('#cancelar');
+const todoList = document.querySelector('#list-todo');
+const todoFiltro = document.querySelector('#todo-filtro');
+const todoEdit = document.querySelector('#edit-form');
+const todoPesquisar = document.querySelector('#pesquisa-form');
+const todoAdcTarefa = document.querySelector('#tarefa-form')
+const cancelBtn = document.querySelector('#cancelar')
+const editInput = document.querySelector('#edit')
+const editForm = document.querySelector('#edit-check')
+
+let oldInputValue;
 
 const saveTodo = (text) => {
     const todo = document.createElement('div')
@@ -31,9 +37,51 @@ const saveTodo = (text) => {
     todoList.appendChild(todo);
 
     todoTarefa.value = '';
+    todoTarefa.focus();
 
     console.log(todo)
-}
+};
+
+const toggleForms = () =>{
+    if(todoPesquisar.style.display === "none"){
+        todoPesquisar.style.display = "block";
+    } else{
+        todoPesquisar.style.display = "none";
+    };
+
+    if(todoFiltro.style.display === "none"){
+        todoFiltro.style.display = "block";
+    } else{
+        todoFiltro.style.display = "none";
+    };
+
+    if(todoAdcTarefa.style.display === "none"){
+        todoAdcTarefa.style.display = "block";
+    } else{
+        todoAdcTarefa.style.display = "none";
+    };
+
+    if(todoEdit.style.display === "block"){
+        todoEdit.style.display = "none";
+    } else{
+        todoEdit.style.display = "block";
+    };
+};
+
+
+const updateTodo = (text) => {
+    const todos = document.querySelectorAll(".todo");
+
+    todos.forEach((todo) => {
+        let todoTitle = todo.querySelector('h4')
+        console.log(todoTitle, text);
+
+        if(todoTitle.innerText === oldInputValue){
+            todoTitle.innerText = text;
+        }
+    });
+};
+
 
 
 todoForm.addEventListener('submit', (e) =>{
@@ -43,15 +91,49 @@ todoForm.addEventListener('submit', (e) =>{
 
     if(inputValue){
         saveTodo(inputValue);
-    }
+    };
 });
 
 document.addEventListener('click', (e)=>{
 
     const targetEl = e.target;
     const parentEl = targetEl.closest('div');
+    let todoTitle;
+
+    if(parentEl && parentEl.querySelector('h4')){
+        todoTitle = parentEl.querySelector('h4').innerText;
+    }
 
     if(targetEl.classList.contains('finish-todo')){
         parentEl.classList.toggle('done');
     }
+
+    if(targetEl.classList.contains('remove-todo')){
+        parentEl.remove();
+    }
+
+    if(targetEl.classList.contains('edit-todo')){
+        toggleForms();
+
+        editInput.value = todoTitle;
+        oldInputValue = todoTitle;
+    }
+});
+
+cancelBtn.addEventListener('click', (e) =>{
+    e.preventDefault();
+
+    toggleForms();
+});
+
+editForm.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const editInputValue = editInput.value
+
+    if(editInputValue){
+        updateTodo(editInputValue);
+    }
+    toggleForms();
+
 });
